a6d2da49c1611d4999012419ff2e6d639e51e68e
IZERE JOSHUA <izerejoshua94@gmail.com>
2025-05-18 10:30:45 +0200

    chore: normalize line endings

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
 
-@Controller()
-export class AuthController {}
+@Controller('auth')
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  //Signup Controller
+  @Post('signup')
+  signup() {
+    return this.authService.signup();
+  }
+
+  //Login Controller
+  @Post('login')
+  login() {
+    return this.authService.login();
+  }
+}
